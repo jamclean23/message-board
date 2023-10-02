2b751724b6ee4bc60d9c82221fed947e63f90105
jamclean23 <jamclean23@gmail.com>
2023-10-02 14:01:00 -0400

    Add new post button and styling tweaks

--- public/index/index.js
+++ public/index/index.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+// Javascript for index page
+
+// ====== MAIN ======
+
+addEventListeners();
+
+
+// ====== LISTENERS ======
+
+// Listener for new post button
+
+function addEventListeners () {
+    addNewPostListener();
+}
+
+function addNewPostListener () {
+    const newPostBtn = document.querySelector('.addBtn');
+
+    newPostBtn.addEventListener('click', () => {
+        window.location.href = '/new';
+    })
+}
